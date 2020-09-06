4b41c5c50f13154db1d7fd5ac6313650aa69fe8d
koreatogether <koreatogether@zoho.com>
2020-09-06 11:48:35 +0900

    dht22+millis()

--- lcdsensorstest/lcdsensorstest/lcdsensorstest.ino
+++ lcdsensorstest/lcdsensorstest/lcdsensorstest.ino
@@ -3,7 +3,12 @@
 
 #include <dht.h>  // dht22용 라이브러리 출처 https://github.com/RobTillaart/DHTlib
 
-dht DHT;
+dht DHT; //dht 를 DHT로 쓰겠다 선언
+
+unsigned long cur_time = 0;
+unsigned long pre_time = 0;
+int duration = 0;
+
 
 #define DHT22_PIN 7  // DHT22 신호핀을 7번으로 하겠다 선언 
 
@@ -20,8 +25,10 @@ struct  //DHT22용 스트럭처
 } stat = { 0,0,0,0,0,0,0,0};
 
 void setup() {
-    Serial.begin(115200);  //기본 시리얼 속도 1152000
-    
+    pre_time = millis(); // delay를 없애기위한 밀리함수 setup함수내 선언 
+
+    Serial.begin(115200);  //기본 시리얼 속도 115200    
+    //
     Serial.println("dht22_test.ino");  //DHT22용 setup 선언시작
     Serial.print("LIBRARY VERSION: ");
     Serial.println(DHT_LIB_VERSION);
@@ -33,7 +40,10 @@ void setup() {
 
 void loop()
 {
-    // READ DATA
+    cur_time = millis(); // delay를 없애기 위한 밀리함수 loop함수내 선언 
+    if(cur_time - pre_time >= 7000) {  //현재 시간 - 과거시간 체크를 하는 함수 시작 및 조건 7초마다  1000 = 1초
+    
+    // DHT22 READ DATA
     Serial.print("DHT22, \t");
 
     uint32_t start = micros();
@@ -99,8 +109,12 @@ void loop()
         Serial.print("\t");
         Serial.print(stat.unknown);
         Serial.println("\n");
-    }
-    delay(2000); // 최소 2초의 딜레이가 필요한 DHT22 센서 
+    }    
+    pre_time = cur_time;
 }
+}
+
+
+    //delay(2000); // 최소 2초의 딜레이가 필요한 DHT22 센서 
 
 
