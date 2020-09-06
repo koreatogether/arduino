72e3430ad3f9797ec0b95db668bafa11fcb7cf5c
koreatogether <koreatogether@zoho.com>
2020-09-06 13:10:32 +0900

    addlcd

--- lcdsensorstest/lcdsensorstest/lcdsensorstest.ino
+++ lcdsensorstest/lcdsensorstest/lcdsensorstest.ino
@@ -1,16 +1,20 @@
 // 온습도 센서 DHT22 추가  20년 9월 06일
-// delay 를 없애기 위한 코드 작성  20년 9월 6일  
+// delay 를 없애기 위한 코드 작성  20년 9월 6일
+// 0816 lcd 추가  20sus 9월 6일   
 
 #include <dht.h>  // dht22용 라이브러리 출처 https://github.com/RobTillaart/DHTlib
-
-dht DHT; //dht 를 DHT로 쓰겠다 선언
-
-unsigned long cur_time = 0;
-unsigned long pre_time = 0;
-int duration = 0;
-
+#include <LiquidCrystal.h>
 
 #define DHT22_PIN 7  // DHT22 신호핀을 7번으로 하겠다 선언 
+dht DHT; //dht 를 DHT로 쓰겠다 선언
+
+unsigned long cur_time = 0;  // 밀리함수 변수 선언
+unsigned long pre_time = 0;
+int duration = 0; // 밀리함수 변수 선언 끝 
+
+LiquidCrystal lcd(8,9,10,11,12,13);
+
+
 
 struct  //DHT22용 스트럭처
 {
@@ -25,15 +29,17 @@ struct  //DHT22용 스트럭처
 } stat = { 0,0,0,0,0,0,0,0};  //DHT22용 스트럭처 끝 
 
 void setup() {
-    pre_time = millis(); // delay를 없애기위한 밀리함수 setup함수내 선언 
-
+    
     Serial.begin(115200);  //기본 시리얼 속도 115200    
-    //
+    pre_time = millis(); // delay를 없애기위한 밀리함수 setup함수내 선언     //
     Serial.println("dht22_test.ino");  //DHT22용 setup 선언시작
     Serial.print("LIBRARY VERSION: ");
     Serial.println(DHT_LIB_VERSION);
     Serial.println();
     Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)"); // DHT22 용 setup 선언끝
+    lcd.begin(8,2); // 0816 lcd  8칸 / 2줄 이라고 선언
+    lcd.clear();  // 0816 lcd 사용하기전 화면 청소 
+
 
 }
 
@@ -109,7 +115,15 @@ void loop()
         Serial.print("\t");
         Serial.print(stat.unknown);
         Serial.println("\n");
-    }    
+    }
+    
+    lcd.setCursor(0,0); // 0816 lcd에 나타날 문구 시작 
+    lcd.print(DHT.temperature); 
+    lcd.write(B11011111);    // 특수문자는 print가 아닌 write로 시작해야 한다. 
+    lcd.print("C");
+    lcd.setCursor(0,1);
+    lcd.print(DHT.humidity);
+    lcd.write(B00100101); // 0816 lcd에 나타날 문구 끝 
     pre_time = cur_time;
 }
 }
