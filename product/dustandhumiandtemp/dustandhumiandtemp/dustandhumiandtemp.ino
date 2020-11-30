7079b95948c2b74e173cef7b735f71b12ae40480
koreatogether <koreatogether@zoho.com>
2020-11-30 20:14:17 +0900

    20-11-30 DustAndHumiAndTemp

--- product/dustandhumiandtemp/dustandhumiandtemp/dustandhumiandtemp.ino
+++ product/dustandhumiandtemp/dustandhumiandtemp/dustandhumiandtemp.ino
@@ -9,23 +9,36 @@
  * 
  */
 
+/*  자작품을 위해서 디스플레이부분 추가
+    20 - 11 - 30 미흡하지만 구현은 했으며 이를 폴더를 정해서 저장해놓음 
+*/
  
 
 
-
+#include <U8glib.h>
 #include <Wire.h>   // I2C 통신을 위한 아두이노 함수 
 #include <dht.h>    // dht 계열 센서중에 11 과 22를 지원하느 라이브러리
 #include <BCHTS4085.h>  // BCHTS4085 센서 회사에서 제공해주는 라이브러리
 #include <PMS.h>    // 미세먼지센서 PMS계열 시리즈에 대한 라이브러리 < 여기선 PMSA003>
 #include <SoftwareSerial.h>  // 소프트시리얼 라이브러리 
 
+
+//&&&&&&&&&&&&&  디스플레이용 관련 시작 &&&&&&&&&&&&&&&&&&&&&&&//
+
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST); // Fast I2C / TWI  를 썼고 본인 oled에 따라서 삭제후 추가하시면 됩니다. 
+
+//&&&&&&&&&&&&&& 디스플레이용 관련 시작 끝 &&&&&&&&&&&&&&&&&&&&&//
+
+
+
 unsigned long timenow = 0;
 unsigned long timelast = 0;
 
 // %%%%%%%%%%%%%%%%%% DHT 22 센서 관련 시작 %%%%%%%%%%%%%%%%%%
 dht DHT;
-#define DHT22_PIN 8                   // DHT 22 센서 모듈의 신호핀을 아두이노 D8에 배정 
-struct  // 센서 검사를 위한 것이라고 하는데 이해를못함 
+#define DHT22_PIN 7                   // DHT 22 센서 모듈의 신호핀을 아두이노 나노 D7에 배정 
+/*struct  // 센서 검사를 위한 것이라고 하는데 이해를못함 
 {
     uint32_t ;
     uint32_t ok;
@@ -35,9 +48,15 @@ struct  // 센서 검사를 위한 것이라고 하는데 이해를못함
     uint32_t ack_l;
     uint32_t ack_h;
     uint32_t unknown;
-} stat = { 0,0,0,0,0,0,0,0};
+}
+stat = 
+{ 
+  0,0,0,0,0,0,0,0
+};
+*/
 // %%%%%%%%%%%%%%%%%% DHT 22 센서 관련 끝 %%%%%%%%%%%%%%%%%%
 
+/*   여기선 필요 없어서 주석처리 
 // ************* BCHTS4085 관련 시작****************** 
 #define BCHTS4085_ADDR 0x40 // BCHTS4085 센서 모듈 주소지정
 #define ALERTPIN 7                    // BCHTS4085 센서 모듈의  INT 핀과 연결되는 아두이노 D7핀 
@@ -46,10 +65,15 @@ void readSensorData(void);
 BCHTS4085 bchts4085 (BCHTS4085_ADDR); //HDC2010 Temperature and Humidity Sensor
 // ************ BCHTS4085 관련 끝  *****************
 
+*/
+
+/*   여기선 필요 없어서 주석처리 
 // ************ TMP36 관련 시작 ****************
 int tmp36Pin = A0;
 float tmp36temperatureC; 
 // ************ TMP36 관련 끝 ****************
+
+*/
 
 //@@@@@@@@@@@@@ PMSA003 미세먼지센서 관련 시작 @@@@@@@@@@@@@@
 SoftwareSerial myserial( 5, 6 );
@@ -58,7 +82,7 @@ PMS::DATA data;
 
 
 const int repeattime = 10000;
-const int readingtime = 3000;   // 측정 간격시간 
+const int readingtime = 10000;   // 측정 간격시간 
 
 unsigned long timerlast = 0;
 unsigned long timernow = 0;
@@ -72,11 +96,12 @@ bool pms_power = false;
 int cycles = 0;
 //@@@@@@@@@@@@@ PMSA003 미세먼지센서 관련 끝 @@@@@@@@@@@@@@
 
+/*   여기선 필요 없어서 주석처리 
 //^^^^^^^^^^^^^ 조도센서  TEMT6000 관련 시작 ^^^^^^^^^^^^^^^^
 #define LIGHTPIN A1
 float readA1 ;
 //^^^^^^^^^^^^^ 조도센서  TEMT6000 관련 끝 ^^^^^^^^^^^^^^^^
-
+*/
 
 
 void setup() 
@@ -88,14 +113,14 @@ void setup()
     }
     
   Wire.begin();  // i2c 통신위한것 없으면 i2c 통신 안된다고 알고 있음 
-  sensorInitialize();  // BCHTS4085 센서 초기화 함수 
+  //sensorInitialize();  // BCHTS4085 센서 초기화 함수 
 
   
   myserial.begin(9600);  // PMSA003 소프트시리얼 통신 속도 
   pms.passiveMode();     // 함수에서 지원하는 작동모드 중 패시브모드 
   pms.sleep();           // PMS 작동 시작을 휴면 모드로 설정 
 
-  pinMode(LIGHTPIN, INPUT);  // 아두이노 A1값을 입력 핀으로 설정
+  //pinMode(LIGHTPIN, INPUT);  // 아두이노 A1값을 입력 핀으로 설정
 
 }  
 
@@ -108,12 +133,12 @@ void loop()
       timelast = timenow ;
       dht22start();  // dht22 센서 읽기 함수 
       
-      readSensorData();   // BCHTS4085 센서 읽기 함수 
-      Serial.println();   // BCHTS4085센서 읽기 함수 끝나고 빈줄 추가 
+    //  readSensorData();   // BCHTS4085 센서 읽기 함수 
+    //  Serial.println();   // BCHTS4085센서 읽기 함수 끝나고 빈줄 추가 
 
-      tmp36(); // tmp36센서의 값 처리 함수 호출 
+    //  tmp36(); // tmp36센서의 값 처리 함수 호출 
 
-      TEMT6000();
+    //  TEMT6000();
     }
   
   pmsa003();  // PMS 계열 먼지센서 값 처리 함수 호출 , 처리 시간이 위 함수들과 안 맞아서 함수내의 시간에 따라 출력    
@@ -121,6 +146,8 @@ void loop()
   
 }
 
+
+/*
 // ********************************************************************
 //********************  BCHTS4085  함수 시작 ***************************
 void sensorInitialize(void){   // BCHTS4085 센서 초기화에 대한 함수 
@@ -174,6 +201,7 @@ void tmp36(void)
  // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 // &&&&&&&&&&&&&&&&&&&&&&&  TMP36 센서용  루프내  함수 끝 &&&&&&&&&&&&&&&&&&&&
 
+*/
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //%%%%%%%%%%%%%%%%%%%%%%%% DHT 22 센서용 루프 함수 시작 %%%%%%%%%%%%%%%%%%%
 void dht22start(void) 
@@ -185,8 +213,8 @@ void dht22start(void)
     int chk = DHT.read22(DHT22_PIN);
     uint32_t stop = micros();
 
-    stat.++;
-    switch (chk)
+    //stat.++;
+    /*switch (chk)
     {
     case DHTLIB_OK:
         stat.ok++;
@@ -216,7 +244,7 @@ void dht22start(void)
         stat.unknown++;
         Serial.print("Unknown error,\t");
         break;
-    }
+    }*/
     // DISPLAY DATA
     Serial.println("dht22_test.ino");
   Serial.print("LIBRARY VERSION: ");
@@ -236,7 +264,7 @@ void dht22start(void)
     Serial.println();
     
 
-    if (stat. % 20 == 0)
+   /* if (stat. % 20 == 0)
     {
         Serial.println("\nTOT\tOK\tCRC\tTO\tCON\tACK_L\tACK_H\tUNK");
         Serial.print(stat.);
@@ -255,7 +283,7 @@ void dht22start(void)
         Serial.print("\t");
         Serial.print(stat.unknown);
         Serial.println("\n");
-    }
+    }*/
 }
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -291,6 +319,28 @@ void dht22start(void)
         {
             Serial.println("No data.");
         }
+
+
+        u8g.firstPage();
+    do
+    {
+    u8g.setFont(u8g_font_helvB18);
+        
+    u8g.setPrintPos(0, 21 );
+    u8g.print("PM2.5:");
+    u8g.setPrintPos(85, 21 );
+    u8g.print(data.PM_AE_UG_2_5);
+    
+    
+    u8g.setPrintPos(5, 58 );
+    u8g.print(DHT.humidity , 0);
+    u8g.drawStr(32,58,"%");        
+      
+    u8g.setPrintPos(75, 58 );
+    u8g.print(DHT.temperature , 0);           
+    u8g.drawStr(105,58,"C");        
+
+    }while(u8g.nextPage());
     }
     
  void pmsa003()
@@ -344,6 +394,7 @@ void dht22start(void)
       //pms_power = false;     
   }
 
+/*
 void TEMT6000(void)
 {
   readA1 = analogRead(LIGHTPIN);
@@ -355,7 +406,12 @@ void TEMT6000(void)
   //Serial.print (convertdigitalsignalA2  );
 }
     
-   
+
+  */
+
+
+ 
+  
     
 
     
